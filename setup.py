83725168d4ae5dc0310841148cee90b5f19d805a
Roba Olana <51477157+robaolana@users.noreply.github.com>
2024-12-23 14:33:52 +0400

    Testing GitHub Actions Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gini-py',
-    version='0.1.5',
+    version='0.1.6',
     author='Roba Olana',
     author_email='support@gini.works',
     description='Python SDK to interact with Gini',
